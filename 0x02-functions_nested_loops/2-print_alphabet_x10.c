fc244ee4dc459cb167a6b7525bc0066cd6961d37
MaxwellMathe <ikbmathek@gmail.com>
2023-07-19 02:24:50 -0700

    hello

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print all alphabet in lowercase
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-        char letter;
-        for (letter  = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-        _putchar('\n');
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }
